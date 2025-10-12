8c6dfb986888f75fd3946624be5959b230922036
thiagoeu <araujo.thiago1051@gmail.com>
2025-10-12 01:35:53 -0300

    user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,12 @@
-import { IsEmail, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'E-mail é obrigatório' })
   @IsEmail({}, { message: 'E-mail inválido' })
   email: string;
 
+  @IsNotEmpty({ message: 'Senha é obrigatória' })
+  @IsString({ message: 'A senha deve ser uma string' })
   @MinLength(5, { message: 'A senha deve ter pelo menos 5 caracteres' })
   password: string;
 }
