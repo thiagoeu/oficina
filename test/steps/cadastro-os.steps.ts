7bbdedecc75b33a9231bdefdf09faa471210460d
thiagoeu <araujo.thiago1051@gmail.com>
2025-10-17 00:33:28 -0300

    fix: adicionado logs dos testez

--- test/steps/cadastro-os.steps.ts
+++ test/steps/cadastro-os.steps.ts
@@ -70,6 +70,7 @@ defineFeature(feature, (test) => {
     }
   }
 
+  // Cenário 1: Criar ordem de serviço completa com peça e serviço válidos
   test('Criar ordem de serviço completa com peça e serviço válidos', ({
     given,
     and,
@@ -119,7 +120,7 @@ defineFeature(feature, (test) => {
       };
 
       const res = await request(server).post('/mechanic').send(mechanicPayload);
-      console.log(res.body);
+
       createdMechanicId = res.body?.mechanic?.id ?? res.body?.id;
       expect(createdMechanicId).toBeDefined();
     });
@@ -136,6 +137,9 @@ defineFeature(feature, (test) => {
         payload.mechanic_id = createdMechanicId;
 
         response = await request(server).post(path).send(payload);
+        console.log(
+          'Cenário 1: Criar ordem de serviço completa com peça e serviço válidos',
+        );
       },
     );
 
@@ -151,82 +155,7 @@ defineFeature(feature, (test) => {
     });
   });
 
-  test('Criar OS com peça fora de estoque', ({ given, and, when, then }) => {
-    given('que o banco de dados de teste está limpo', async () => {
-      await prepareDatabase();
-    });
-
-    and('existe um cliente previamente cadastrado', async () => {
-      const customerPayload = {
-        name: 'Maria',
-        lastName: 'Silva',
-        cpf: '12345678901',
-        phone: '11999999999',
-        zipCode: '12345678',
-        user: {
-          email: 'maria@teste.com',
-          password: 'senha123',
-        },
-      };
-
-      const res = await request(server).post('/customer').send(customerPayload);
-      createdCustomerId = res.body?.customer?.id;
-      expect(createdCustomerId).toBeDefined();
-    });
-
-    and('existe um veículo previamente cadastrado', async () => {
-      const vehiclePayload = {
-        model: 'Gol',
-        brand: 'Volkswagen',
-        color: 'Prata',
-        year: 2015,
-        plate: 'ABC1234',
-        customerId: createdCustomerId,
-      };
-
-      const res = await request(server).post('/vehicle').send(vehiclePayload);
-      createdVehicleId = res.body?.vehicle?.id ?? res.body?.id;
-      expect(createdVehicleId).toBeDefined();
-    });
-
-    and('existe um mecânico previamente cadastrado', async () => {
-      const mechanicPayload = {
-        name: 'João Mecânico',
-      };
-
-      const res = await request(server).post('/mechanic').send(mechanicPayload);
-      console.log(res.body);
-      createdMechanicId = res.body?.mechanic?.id ?? res.body?.id;
-      expect(createdMechanicId).toBeDefined();
-    });
-
-    when(
-      /^eu envio uma requisição POST para "(.*)" com:$/,
-      async (path, table) => {
-        const row = table[0];
-        const payload = mapTableToPayload(row);
-
-        // Substituir os valores dos IDs para garantir os vinculados acima
-        payload.customer_id = createdCustomerId;
-        payload.vehicle_id = createdVehicleId;
-        payload.mechanic_id = createdMechanicId;
-
-        response = await request(server).post(path).send(payload);
-      },
-    );
-
-    then('o sistema deve retornar status 404', () => {
-      expect(response.status).toBe(404);
-    });
-
-    and(/^o corpo deve conter a mensagem "(.*)"$/, (msg) => {
-      const msgBody = Array.isArray(response.body.message)
-        ? response.body.message[0]
-        : response.body.message;
-      expect(msgBody).toMatch(new RegExp(msg, 'i'));
-    });
-  });
-
+  // Cenário 6 : Criar OS apenas com peça (sem serviço)
   test('Criar OS apenas com Peça (sem serviço)', ({
     given,
     and,
@@ -276,7 +205,7 @@ defineFeature(feature, (test) => {
       };
 
       const res = await request(server).post('/mechanic').send(mechanicPayload);
-      console.log(res.body);
+
       createdMechanicId = res.body?.mechanic?.id ?? res.body?.id;
       expect(createdMechanicId).toBeDefined();
     });
@@ -293,8 +222,7 @@ defineFeature(feature, (test) => {
         payload.mechanic_id = createdMechanicId;
 
         response = await request(server).post(path).send(payload);
-        console.log('Status:', response.status);
-        console.log('Body:', response.body);
+        console.log('Cenário 6: Criar OS apenas com Peça (sem serviço)');
       },
     );
 
@@ -310,6 +238,85 @@ defineFeature(feature, (test) => {
     });
   });
 
+  // Cenário 7: Criar OS com peça fora de estoque
+  test('Criar OS com peça fora de estoque', ({ given, and, when, then }) => {
+    given('que o banco de dados de teste está limpo', async () => {
+      await prepareDatabase();
+    });
+
+    and('existe um cliente previamente cadastrado', async () => {
+      const customerPayload = {
+        name: 'Maria',
+        lastName: 'Silva',
+        cpf: '12345678901',
+        phone: '11999999999',
+        zipCode: '12345678',
+        user: {
+          email: 'maria@teste.com',
+          password: 'senha123',
+        },
+      };
+
+      const res = await request(server).post('/customer').send(customerPayload);
+      createdCustomerId = res.body?.customer?.id;
+      expect(createdCustomerId).toBeDefined();
+    });
+
+    and('existe um veículo previamente cadastrado', async () => {
+      const vehiclePayload = {
+        model: 'Gol',
+        brand: 'Volkswagen',
+        color: 'Prata',
+        year: 2015,
+        plate: 'ABC1234',
+        customerId: createdCustomerId,
+      };
+
+      const res = await request(server).post('/vehicle').send(vehiclePayload);
+      createdVehicleId = res.body?.vehicle?.id ?? res.body?.id;
+      expect(createdVehicleId).toBeDefined();
+    });
+
+    and('existe um mecânico previamente cadastrado', async () => {
+      const mechanicPayload = {
+        name: 'João Mecânico',
+      };
+
+      const res = await request(server).post('/mechanic').send(mechanicPayload);
+
+      createdMechanicId = res.body?.mechanic?.id ?? res.body?.id;
+      expect(createdMechanicId).toBeDefined();
+    });
+
+    when(
+      /^eu envio uma requisição POST para "(.*)" com:$/,
+      async (path, table) => {
+        const row = table[0];
+        const payload = mapTableToPayload(row);
+
+        // Substituir os valores dos IDs para garantir os vinculados acima
+        payload.customer_id = createdCustomerId;
+        payload.vehicle_id = createdVehicleId;
+        payload.mechanic_id = createdMechanicId;
+
+        response = await request(server).post(path).send(payload);
+        console.log('Cenário 7: Criar OS com peça fora de estoque');
+      },
+    );
+
+    then('o sistema deve retornar status 404', () => {
+      expect(response.status).toBe(404);
+    });
+
+    and(/^o corpo deve conter a mensagem "(.*)"$/, (msg) => {
+      const msgBody = Array.isArray(response.body.message)
+        ? response.body.message[0]
+        : response.body.message;
+      expect(msgBody).toMatch(new RegExp(msg, 'i'));
+    });
+  });
+
+  // Cenário 9:  Criar OS com valor de pagamento inconsistente (valor negativo ou zero)
   test('Criar OS com valor de pagamento inconsistente (valor negativo ou zero)', ({
     given,
     and,
@@ -359,7 +366,7 @@ defineFeature(feature, (test) => {
       };
 
       const res = await request(server).post('/mechanic').send(mechanicPayload);
-      console.log(res.body);
+
       createdMechanicId = res.body?.mechanic?.id ?? res.body?.id;
       expect(createdMechanicId).toBeDefined();
     });
@@ -376,6 +383,9 @@ defineFeature(feature, (test) => {
         payload.mechanic_id = createdMechanicId;
 
         response = await request(server).post(path).send(payload);
+        console.log(
+          'Cenário 9: Criar OS com valor de pagamento inconsistente (valor negativo ou zero)',
+        );
       },
     );
 
